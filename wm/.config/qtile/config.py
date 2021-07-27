8dd23d8c79add8ef405cd843eb1c652d13041905
Alexander Neville <git@afneville.com>
2021-07-27 09:07:40 +0100

    new colourschemes

--- wm/.config/qtile/config.py
+++ wm/.config/qtile/config.py
@@ -1,34 +1,14 @@
-# Copyright (c) 2010 Aldo Cortesi
-# Copyright (c) 2010, 2014 dequis
-# Copyright (c) 2012 Randall Ma
-# Copyright (c) 2012-2014 Tycho Andersen
-# Copyright (c) 2012 Craig Barnes
-# Copyright (c) 2013 horsik
-# Copyright (c) 2013 Tao Sauvage
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-from typing import List  # noqa: F401
-
-from libqtile import bar, layout, widget
-from libqtile.config import Click, Drag, Group, Key, Match, Screen
+# -*- coding: utf-8 -*-
+import os
+import re
+import socket
+import subprocess
+from libqtile import qtile
+from libqtile.config import Click, Drag, Group, KeyChord, Key, Match, Screen
+from libqtile.command import lazy
+from libqtile import layout, bar, widget, hook
 from libqtile.lazy import lazy
+from typing import List  # noqa: F401
 
 mod = "mod4"
 terminal = "alacritty"
@@ -86,123 +66,140 @@ keys = [
     Key([], "XF86AudioMute", lazy.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle")),
     Key([], "XF86AudioLowerVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ -10%")),
     Key([], "XF86AudioRaiseVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ +10%")),
+
 ]
 
 groups = [Group(i) for i in "123456789"]
 
 for i in groups:
     keys.extend([
-        # mod1 + letter of group = switch to group
-        Key([mod], i.name, lazy.group[i.name].toscreen(),
-            desc="Switch to group {}".format(i.name)),
-
-        # mod1 + shift + letter of group = switch to & move focused window to group
-        #Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
-            # desc="Switch to & move focused window to group {}".format(i.name)),
-        # Or, use below if you prefer not to switch to that group.
-        # # mod1 + shift + letter of group = move focused window to group
-        Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            desc="move focused window to group {}".format(i.name)),
+        Key([mod], i.name, lazy.group[i.name].toscreen(), desc="Switch to group {}".format(i.name)),
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name), desc="move focused window to group {}".format(i.name)),
     ])
-# 0: unfocused colour 1: blue for tiling focus 2: black for fullscreen focus. 3: optional grey colour for borders.
+
 window_colours = ['#adb1b5', '#51afef', '#000000', '#333333']
-# 0: grey for the bar background 1: yellow for clock. 2: blue for tags 3: red for mem widget 4: green for cpu 5: purple 6: dull white for inactive tags
-bar_colours = ['#222222', '#dcc143', '#3d7abc', '#d95a2a', '#99dc43', '#a952e8', '#b1adad']
-#my_border_width=1
 
 layouts = [
-    # For gaps use these layouts.
-    #layout.Columns(border_width=1, border_focus=window_colours[1], border_focus_stack=window_colours[1], border_on_single=True, margin=[5, 5, 0, 0]),
-    #layout.Stack(border_width=1, border_focus=window_colours[2], num_stacks=1, margin=[5, 5, 0, 0]),#fullscreen emulation
-    layout.Columns(border_width=2, border_focus=window_colours[3], border_focus_stack=window_colours[3], border_normal=window_colours[2], border_on_single=True),
-    layout.Stack(border_width=0, border_focus=window_colours[2], num_stacks=1),#fullscreen emulation
-    # I am going to use the stack layout to emulate fullscreen
-    # layout.Max(),
-    # Try more layouts by unleashing below layouts.
-    # layout.Bsp(),
-    # layout.Matrix(),
-    # layout.MonadTall(),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
+    layout.Columns(border_width=1, border_focus=window_colours[1], border_focus_stack=window_colours[1], border_on_single=True, margin=[0, 0, 0, 0]),
+    layout.Stack(border_width=1, border_focus=window_colours[2], num_stacks=1, margin=[0, 0, 0, 0]),
 ]
 
-widget_defaults = dict(
-    font='Mononoki Nerd Font',
-    fontsize=16,
-    padding=3,
-)
+colors = [["#282c34", "#282c34"], # panel background
+          ["#3d3f4b", "#434758"], # background for current screen tab
+          ["#ffffff", "#ffffff"], # font color for group names
+          ["#ff5555", "#ff5555"], # border line color for current tab
+          ["#74438f", "#74438f"], # border line color for 'other tabs' and color for 'odd widgets'
+          ["#4f76c7", "#4f76c7"], # color for the 'even widgets'
+          ["#e1acff", "#e1acff"], # window name
+          ["#ecbbfb", "#ecbbfb"]] # backbround for inactive screens
+
+# prompt = "{0}@{1}: ".format(os.environ["USER"], socket.gethostname())
+
+##### DEFAULT WIDGET SETTINGS #####
+widget_defaults = dict(font="Jetbrains Mono Nerd Font", fontsize = 12, padding = 0, background=colors[2])
+
 extension_defaults = widget_defaults.copy()
+
 screens = [
     Screen(
         top=bar.Bar(
             [
-                widget.GroupBox(disable_drag=True, font="mononoki nerd font", use_mouse_wheel=False, highlight_method="block", this_current_screen_border=bar_colours[2], this_screen_border=bar_colours[2], inactive=bar_colours[6], rounded=False, hide_unused=False),
-                #widget.Spacer(),
-                #widget.Clock(format='%d-%m-%Y %H:%M:%S', background=bar_colours[0], foreground=bar_colours[6]),
-                #widget.Spacer(),
-                #widget.CPU(format='{freq_current}ghz', background=bar_colours[5], foreground=bar_colours[0]),
-                #widget.TextBox(" "),
-                #widget.CPU(format='{load_percent}%', background=bar_colours[4], foreground=bar_colours[0]),
-                #widget.TextBox(" "),
-                #widget.Memory(format='{MemUsed: .0f} MB', background=bar_colours[3], foreground=bar_colours[0]),
-                #widget.TextBox(" "),
-                #widget.CheckUpdates(no_update_string="Updates: 0", custom_command="expac -S -H M '%k\t%n' $(pacman -Qqu) | sort -sh", background=bar_colours[2], foreground=bar_colours[0], colour_have_updates=bar_colours[0], colour_no_updates=bar_colours[0]),
-                #widget.TextBox(" "),
-                #widget.TextBox(" "),
-                #widget.WindowCount(),
-                #widget.TextBox(" "),
-                #widget.CurrentLayoutIcon(scale=0.7),
+                widget.Sep( linewidth = 0, padding = 6, foreground = colors[2], background = colors[0]),
+                # widget.Image( filename = "~/.config/qtile/icons/python-white.png", scale = "False", mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(myTerm)}), 
+                widget.Sep( linewidth = 0, padding = 6, foreground = colors[2], background = colors[0]),
+                widget.GroupBox( font = "Ubuntu Bold", fontsize = 9, margin_y = 3, margin_x = 0, padding_y = 5, padding_x = 3, borderwidth = 3, active = colors[2], inactive = colors[7], rounded = False, highlight_color = colors[1], highlight_method = "line", this_current_screen_border = colors[6], this_screen_border = colors [4], other_current_screen_border = colors[6], other_screen_border = colors[4], foreground = colors[2], background = colors[0]),
+                # widget.Prompt( prompt = prompt, font = "Ubuntu Mono", padding = 10, foreground = colors[3], background = colors[1]),
+                widget.Sep( linewidth = 0, padding = 40, foreground = colors[2], background = colors[0]),
+                widget.Spacer(background=colors[0]),
+                # widget.WindowName( foreground = colors[6], background = colors[0], padding = 0),
+                # widget.Systray( background = colors[0], padding = 5),
+                # widget.TextBox( text = '', background = colors[0], foreground = colors[4], padding = 0, fontsize = 37),
+                # widget.Net( interface = "enp3s0", format = '{down} ↓↑ {up}', foreground = colors[2], background = colors[4], padding = 5),
+                # widget.TextBox( text = '', background = colors[4], foreground = colors[5], padding = 0, fontsize = 37),
+                # widget.TextBox( text = " 🌡 TEMP NOT SHOWN ", padding = 2, foreground = colors[2], background = colors[5], fontsize = 11),
+                # widget.TextBox( text='', background = colors[5], foreground = colors[4], padding = 0, fontsize = 37),
+                # widget.TextBox( text = " ⟳", padding = 2, foreground = colors[2], background = colors[4], fontsize = 14),
+                # widget.CheckUpdates( update_interval = 1800, distro = "Arch_checkupdates", display_format = "{updates} Updates", foreground = colors[2], mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(myTerm + ' -e sudo pacman -Syu')}, background = colors[4]),
+                # widget.TextBox( text = '', background = colors[4], foreground = colors[5], padding = 0, fontsize = 37),
+                # widget.TextBox( text = " 🖬", foreground = colors[2], background = colors[5], padding = 0, fontsize = 14),
+                # widget.Memory( foreground = colors[2], background = colors[5], mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(myTerm + ' -e htop')}, padding = 5),
+                # widget.TextBox( text='', background = colors[5], foreground = colors[4], padding = 0, fontsize = 37),
+                # widget.TextBox( text = " ₿", padding = 0, foreground = colors[2], background = colors[4], fontsize = 12),
+                # widget.BitcoinTicker( foreground = colors[2], background = colors[4], padding = 5),
+                # widget.TextBox( text = '', background = colors[4], foreground = colors[5], padding = 0, fontsize = 37),
+                # widget.TextBox( text = " Vol:", foreground = colors[2], background = colors[5], padding = 0),
+                # widget.Volume( foreground = colors[2], background = colors[5], padding = 5),
+                widget.TextBox( text = '', background = colors[5], foreground = colors[4], padding = 0, fontsize = 37),
+                # widget.CurrentLayoutIcon( custom_icon_paths = [os.path.expanduser("~/.config/qtile/icons")], foreground = colors[0], background = colors[4], padding = 0, scale = 0.7),
+                widget.CurrentLayout( foreground = colors[2], background = colors[4], padding = 5),
+                widget.TextBox( text = '', background = colors[4], foreground = colors[5], padding = 0, fontsize = 37),
+                widget.Sep(linewidth = 0, padding = 6, foreground = colors[5], background = colors[5]),
+                widget.Clock(foreground = colors[2], background = colors[5], format = "%A, %B %d - %H:%M "),
             ],
-            24,
-            opacity=0.8,
-            background=bar_colours[0]
-        ),
-        # For easy toggling
-        #bottom=bar.Bar([], 5 ,opacity=0.0),
-        # For even gaps, put some space to the left and at the bottom.
-        # left=bar.Gap(5),
-        # bottom=bar.Gap(5)
-    ),
+            opacity=1.0,
+            size=20,
+            background=colors[0]
+        )
+    )
 ]
 
-# Drag floating layouts.
+# def window_to_prev_group(qtile):
+#     if qtile.currentWindow is not None:
+#         i = qtile.groups.index(qtile.currentGroup)
+#         qtile.currentWindow.togroup(qtile.groups[i - 1].name)
+# 
+# def window_to_next_group(qtile):
+#     if qtile.currentWindow is not None:
+#         i = qtile.groups.index(qtile.currentGroup)
+#         qtile.currentWindow.togroup(qtile.groups[i + 1].name)
+# 
+# def window_to_previous_screen(qtile):
+#     i = qtile.screens.index(qtile.current_screen)
+#     if i != 0:
+#         group = qtile.screens[i - 1].group.name
+#         qtile.current_window.togroup(group)
+# 
+# def window_to_next_screen(qtile):
+#     i = qtile.screens.index(qtile.current_screen)
+#     if i + 1 != len(qtile.screens):
+#         group = qtile.screens[i + 1].group.name
+#         qtile.current_window.togroup(group)
+# 
+# def switch_screens(qtile):
+#     i = qtile.screens.index(qtile.current_screen)
+#     group = qtile.screens[i - 1].group
+#     qtile.current_screen.set_group(group)
+
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(),
-         start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(),
-         start=lazy.window.get_size()),
+    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
     Click([mod], "Button2", lazy.window.bring_to_front())
 ]
 
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: List
-main = None  # WARNING: this is deprecated and will be removed soon
+main = None
 follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = True
+
 floating_layout = layout.Floating(float_rules=[
     # Run the utility of `xprop` to see the wm class and name of an X client.
+    # default_float_rules include: utility, notification, toolbar, splash, dialog,
+    # file_progress, confirm, download and error.
     *layout.Floating.default_float_rules,
-    Match(wm_class='confirmreset'),  # gitk
-    Match(wm_class='makebranch'),  # gitk
-    Match(wm_class='maketag'),  # gitk
-    Match(wm_class='ssh-askpass'),  # ssh-askpass
-    Match(title='branchdialog'),  # gitk
-    Match(title='pinentry'),  # GPG key password entry
+    Match(title='Confirmation'),      # tastyworks exit box
+    Match(title='Qalculate!'),        # qalculate-gtk
+    Match(wm_class='kdenlive'),       # kdenlive
+    Match(wm_class='pinentry-gtk-2'), # GPG key password entry
 ])
+
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
+# @hook.subscribe.startup_once
+# def start_once():
+#     home = os.path.expanduser('~')
+#     subprocess.call([home + '/.config/qtile/autostart.sh'])
+
 wmname = "LG3D"
