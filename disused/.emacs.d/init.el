e86690dabe1f625690d0e7e995f89e673d224ea6
Alexander Neville <git@afneville.com>
2021-12-07 23:18:10 +0000

    worked on vanilla emacs config

--- disused/.emacs.d/init.el
+++ disused/.emacs.d/init.el
@@ -7,10 +7,12 @@
 
 (add-hook 'emacs-startup-hook #'alex/display-startup-time)
 
+(setq package-native-compile t)
+(setq native-comp-async-report-warnings-errors nil)
 (require 'package)
 (setq package-archives '(("melpa" . "http://melpa.org/packages/")
-                         ("org" . "http://orgmode.org/elpa/")
-                         ("elpa" . "http://elpa.gnu.org/packages/")))
+			 ("org" . "http://orgmode.org/elpa/")
+			 ("elpa" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
@@ -20,31 +22,27 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; Auto generated stuff, see my config below
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(company-box-doc-enable nil)
- '(company-box-scrollbar nil)
- '(evil-want-keybinding nil)
- '(lsp-enable-snippet nil)
- '(org-directory "~/code
-/org")
- '(org-startup-folded t)
- '(package-selected-packages
-   '(haskell-mode toc-org evil-org yasnippet company-tabnine undo-tree nlinum lua-mode typescript-mode web-mode json-mode exec-path-from-shell all-the-icons-dired dired-single evil-magit magit visual-fill-column org-bullets org-mode yasnippet-snippets treemacs-all-the-icons treemacs-projectile lsp-treemacs projectile treemacs-evil python-mode company-lsp ivy-rich eglot lsp-jedi elpy company-box company lsp-mode hydra evil-collection general which-key rainbow-delimiters doom-themes doom-modeline counsel ivy use-package evil))
- '(projectile-mode t nil (projectile)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(fixed-pitch ((t (:family "Roboto Mono" :height 130))))
- '(org-ellipsis ((t (:foreground "dark gray" :underline nil))))
- '(variable-pitch ((t (:family "Roboto" :height 150)))))
-(setq org-agenda-diary-file "~/code/org/agenda.org")
+  ;; Auto generated stuff, see my config below
+  (custom-set-variables
+   ;; custom-set-variables was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   '(evil-want-keybinding nil)
+   '(org-startup-folded t)
+   '(org-directory "~/code/org")
+   '(package-selected-packages
+     '(toc-org evil-org yasnippet undo-tree nlinum exec-path-from-shell all-the-icons-dired dired-single evil-magit magit visual-fill-column org-bullets org-mode yasnippet-snippets ivy-rich evil-collection general which-key rainbow-delimiters doom-themes doom-modeline counsel ivy use-package evil))
+   )
+  (custom-set-faces
+   ;; custom-set-faces was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   '(fixed-pitch ((t (:family "Iosevka Nerd Font" :height 110))))
+   '(org-ellipsis ((t (:foreground "dark gray" :underline nil))))
+   '(variable-pitch ((t (:family "Roboto" :height 130)))))
+  (setq org-agenda-diary-file "~/code/org/agenda.org")
 
 (tool-bar-mode -1)
 (tooltip-mode -1)
@@ -68,10 +66,11 @@
   (setq explicit-shell-file-name "bash"))
 
 (setq scroll-conservatively 101)
-(setq scroll-margin 1)
+(setq scroll-margin 5)
 (setq redisplay-dont-pause t)
 (setq jit-lock-defer-time 0)
-;(setq fast-but-imprecise-scrolling t)
+(setq scroll-preserve-screen-position 1)
+(setq fast-but-imprecise-scrolling nil)
 
 (setq mouse-wheel-scroll-amount '(3 ((shift) . 1))) ;; one line at a time
 (setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
@@ -81,10 +80,10 @@
 ;(use-package smooth-scrolling)
 ;(smooth-scrolling-mode 1)
 
-(defun alex/font-faces ()
-  (set-face-attribute 'default nil :font "Roboto Mono" :height 130)
-  (set-face-attribute 'fixed-pitch nil :font "Roboto Mono" :height 130)
-  (set-face-attribute 'variable-pitch nil :font "Roboto" :height 150 :weight 'regular))
+  (defun alex/font-faces ()
+    (set-face-attribute 'default nil :font "Iosevka Nerd Font" :height 110)
+    (set-face-attribute 'fixed-pitch nil :font "Iosevka Nerd Font" :height 110)
+    (set-face-attribute 'variable-pitch nil :font "Roboto" :height 130 :weight 'regular))
 
 (if (daemonp)
     (add-hook 'after-make-frame-functions
@@ -111,19 +110,22 @@
 (set-display-table-slot standard-display-table 'truncation 32)
 
 (dolist (mode '(org-mode-hook
-                term-mode-hook
-                eshell-mode-hook))
+		term-mode-hook
+		eshell-mode-hook))
   (add-hook mode (lambda () (setq visual-line-mode t))))
 
 (use-package all-the-icons)
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 35)))
+  :custom ((doom-modeline-height 25)))
+(setq doom-modeline-height 1)
+(set-face-attribute 'mode-line nil :family "Iosevka Nerd Font" :height 110)
+(set-face-attribute 'mode-line-inactive nil :family "Iosevka Nerd Font" :height 110)
 (setq doom-modeline-enable-word-count nil)
 (setq doom-modeline-buffer-encoding nil)
 (use-package doom-themes)
-(load-theme 'doom-one t)
+(load-theme 'doom-nord t)
 
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
@@ -141,7 +143,7 @@
   :diminish
   :bind (("C-s" . swiper)
          :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)	
+         ("TAB" . ivy-alt-done)
          ("C-l" . ivy-alt-done)
          ("C-j" . ivy-next-line)
          ("C-k" . ivy-previous-line)
@@ -172,7 +174,7 @@
 
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
-(global-set-key (kbd "C-u") 'tab-to-tab-stop)
+;; (global-set-key (kbd "C-u") 'tab-to-tab-stop)
 
 (use-package general
   :after evil
@@ -187,7 +189,6 @@
    "ff" '(counsel-find-file :which-key "quick-find")
    "ft" '(treemacs :which-key "toggle treemacs")
    "fs" '(lsp-treemacs-symbols :which-key "toggle lsp symbols")
-   "fp" '(projectile-switch-project :which-key "open project")
    "fd" '(dired-jump :which-key "dired-mode"))
   (alex/leader-keys
     "a" '(:ignore t :which-key "actions")
@@ -221,7 +222,8 @@
     "ob" '(org-babel-tangle :which-key "export blocks")
     "ol" '(org-store-link :which-key "store link")
     "oi" '(org-insert-last-stored-link :which-key "insert link")
-    "or" '(org-mode-restart :which-key "reload"))
+    "or" '(org-mode-restart :which-key "restart")
+    "oR" '(org-reload :which-key "reload"))
   (alex/leader-keys
     "s" '(swiper :which-key "search this file")
     "m" '(magit-status :which-key "magit")
@@ -245,8 +247,8 @@
 (use-package evil
   :init
   (setq evil-want-integration t)
-  (setq evil-want-fine-undo 'fine)  
-  ;(setq evil-want-C-u-scroll t); Use this option if you want C-u to scroll. I do not.
+  (setq evil-want-fine-undo 'fine)
+  (setq evil-want-C-u-scroll t)
   :config
   (evil-mode 1)
   (evil-set-undo-system 'undo-tree)
@@ -254,6 +256,7 @@
   (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
   (evil-global-set-key 'motion "j" 'evil-next-visual-line)
   (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+  (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
   (define-key evil-normal-state-map (kbd "C-j") 'counsel-ibuffer)
   (evil-set-initial-state 'messages-buffer-mode 'normal)
   (evil-set-initial-state 'dashboard-mode 'normal)
@@ -275,23 +278,23 @@
   (global-undo-tree-mode))
 
 (defun alex/evil-write ()
-    (interactive)  
+    (interactive)
     (save-buffer)
     (kill-this-buffer))
 (evil-ex-define-cmd "wq" 'alex/evil-write)
 (evil-ex-define-cmd "q" 'kill-this-buffer)
 
-(use-package projectile
-  :diminish projectile-mode
-  :config (projectile-mode)
-  :custom ((projectile-completion-system 'ivy))
-  :bind-keymap
-  ("C-c p" . projectile-command-map)
-  :init
-  ;; NOTE: Set this to the folder where you keep your Git repos!
-  (when (file-directory-p "~/code")
-    (setq projectile-project-search-path '("~/code")))
-  (setq projectile-switch-project-action #'projectile-dired))
+;; (use-package projectile
+;;   :diminish projectile-mode
+;;   :config (projectile-mode)
+;;   :custom ((projectile-completion-system 'ivy))
+;;   :bind-keymap
+;;   ("C-c p" . projectile-command-map)
+;;   :init
+;;   ;; NOTE: Set this to the folder where you keep your Git repos!
+;;   (when (file-directory-p "~/code")
+;;     (setq projectile-project-search-path '("~/code")))
+;;   (setq projectile-switch-project-action #'projectile-dired))
 
 (use-package magit
   :custom
@@ -310,6 +313,21 @@
 (use-package all-the-icons-dired
   :hook (dired-mode . all-the-icons-dired-mode))
 
+(use-package page-break-lines)
+(use-package dashboard
+  :ensure t
+  :init
+  (setq dashboard-set-footer nil)
+  (setq dashboard-set-heading-icons t)
+  (setq dashboard-set-file-icons t)
+  (setq dashboard-banner-logo-title "www.gnu.org")
+  (setq dashboard-startup-banner 'official)
+  ;; (setq dashboard-startup-banner 'logo)
+  (setq dashboard-center-content nil)
+  (setq dashboard-items '())
+  :config
+  (dashboard-setup-startup-hook))
+
 (defun alex/org-mode-setup ()
   (org-indent-mode)
   (variable-pitch-mode 1) ;; If you want fancy variable width fonts.
@@ -322,13 +340,13 @@
 
   (dolist (face '((org-level-1 . 1.2)
                   (org-level-2 . 1.1)
-                  (org-level-3 . 1.05)
-                  (org-level-4 . 1.0)
+                  (org-level-3 . 1.1)
+                  (org-level-4 . 1.1)
                   (org-level-5 . 1.1)
                   (org-level-6 . 1.1)
                   (org-level-7 . 1.1)
                   (org-level-8 . 1.1)))
-    (set-face-attribute (car face) nil :font "Roboto" :weight 'regular :height 150))
+    (set-face-attribute (car face) nil :font "Roboto" :weight 'semi-bold :height 1.0))
 
   ;; Ensure that anything that should be fixed-pitch in Org files appears that way
   (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
@@ -341,12 +359,17 @@
 
 (require 'org)
 (setq org-cycle-separator-lines 2)
+(setq org-src-fontify-natively t
+      org-src-window-setup 'current-window
+      org-src-strip-leading-and-trailing-blank-lines t
+      org-src-preserve-indentation t
+      org-edit-src-content-indentation 0
+      org-src-tab-acts-natively t)
 
 (use-package org
   :hook (org-mode . alex/org-mode-setup)
   :config
   ;(setq org-ellipsis " ")
-  ;;(setq org-ellipsis " ⤵")
   (setq org-indent-indentation-per-level 2)
   (setq org-hide-emphasis-markers t)
   (setq org-agenda-files '("~/code/org/agenda.org"))
@@ -366,54 +389,54 @@
  '((emacs-lisp .t)
    (python . t)))
 
-;(use-package org-bullets
-;  :after org
-;  :hook (org-mode . org-bullets-mode)
-;  :custom
-;  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
 
 (defun alex/org-mode-visual-fill ()
-  (setq visual-fill-column-width 180
+  (setq visual-fill-column-width 100
         visual-fill-column-center-text t)
   (visual-fill-column-mode 1))
 
 (use-package visual-fill-column
   :hook (org-mode . alex/org-mode-visual-fill))
 
-(setq lsp-headerline-breadcrumb-enable nil)
-(setq lsp-ui-doc-mode 0)
-(setq lsp-diagnostics-provider :none)
-(setq lsp-signature-auto-activate nil)
-(setq lsp-signature-render-documentation nil)
-(setq lsp-modeline-code-actions-enable nil)
-(setq lsp-modeline-diagnostics-enable nil)
-(setq lsp-log-io nil)
-(setq lsp-restart 'auto-restart)
+;; (setq lsp-headerline-breadcrumb-enable nil)
+;; (setq lsp-ui-doc-mode 0)
+;; (setq lsp-diagnostics-provider :none)
+;; (setq lsp-signature-auto-activate nil)
+;; (setq lsp-signature-render-documentation nil)
+;; (setq lsp-modeline-code-actions-enable nil)
+;; (setq lsp-modeline-diagnostics-enable nil)
+;; (setq lsp-log-io nil)
+;; (setq lsp-restart 'auto-restart)
 
-(use-package lsp-mode
-  :commands (lsp lsp-deferred)
-  :init
-  (setq lsp-keymap-prefix "C-c l")
-  :hook
-  (web-mode . lsp-deferred)
-  :config
-  (lsp-enable-which-key-integration t))
-(require 'lsp-mode)
+  ;; (use-package lsp-mode
+  ;;   :commands (lsp lsp-deferred)
+  ;;   :init
+  ;;   (setq lsp-keymap-prefix "C-c l")
+  ;;   :hook
+  ;;   (web-mode . lsp-deferred)
+  ;;   :config
+  ;;   (lsp-enable-which-key-integration t))
+  ;; (require 'lsp-mode)
 
-(use-package company
-  :after lsp-mode
-  :hook (lsp-mode . company-mode)
-  :config
-  (setq company-selection-wrap-around t)
-  :custom
-  (company-minimum-prefix-length 1)
-  (company-idle-delay 0.0)
-  ; this line is for tab and go completion
-  (company-tng-configure-default))
-(with-eval-after-load 'company
-  ; also use my prefered keys for selection
-  (define-key company-active-map (kbd "C-j") #'company-select-next)
-  (define-key company-active-map (kbd "C-k") #'company-select-previous))
+;; (use-package company
+;;   :after lsp-mode
+;;   :hook (lsp-mode . company-mode)
+;;   :config
+;;   (setq company-selection-wrap-around t)
+;;   :custom
+;;   (company-minimum-prefix-length 1)
+;;   (company-idle-delay 0.0)
+;;   ; this line is for tab and go completion
+;;   (company-tng-configure-default))
+;; (with-eval-after-load 'company
+;;   ; also use my prefered keys for selection
+;;   (define-key company-active-map (kbd "C-j") #'company-select-next)
+;;   (define-key company-active-map (kbd "C-k") #'company-select-previous))
 
 ;(use-package company-box ; This package adds some icons in company mode.
 ;  :after company
@@ -426,33 +449,31 @@
 ;(add-hook 'prog-mode-hook (lambda () (company-mode t)))
 ;;(add-hook 'prog-mode-hook (lambda () (company-tabnine t)))
 
-(use-package lsp-jedi
-  :ensure t)
+;; (use-package lsp-jedi
+;;   :ensure t)
+;; 
+;; (add-hook 'python-mode-hook 'lsp-deferred)
+;; (add-hook 'c++-mode-hook 'lsp-deferred)
+;; (add-hook 'c-mode-hook 'lsp-deferred)
+;; 
+;; (use-package web-mode
+;;   :mode "\\.js\\'"
+;;   :hook (web-mode . lsp-deferred))
 
-(add-hook 'python-mode-hook 'lsp-deferred)
-(add-hook 'c++-mode-hook 'lsp-deferred)
-(add-hook 'c-mode-hook 'lsp-deferred)
+;; (use-package lua-mode
+;;        :hook (lua-mode-hook . lua-mode))
+;; 
+;;  (use-package haskell-mode
+;;        :hook (haskell-mode-hook .haskell-mode))
 
-(use-package web-mode
-  :mode "\\.js\\'"
-  :hook (web-mode . lsp-deferred))
-
-; lua mode for configuring awesome window manager
-(use-package lua-mode
-       :hook (lua-mode-hook . lua-mode))
-   ; Haskell mode for configuring xmonad window manager
-
- (use-package haskell-mode
-       :hook (haskell-mode-hook .haskell-mode))
-
-(use-package lsp-treemacs
-  :after lsp)
-(use-package treemacs-projectile
-  :after (treemacs projectile)
-  :ensure t)
-(use-package treemacs-all-the-icons
-  :after treemacs)
-(use-package treemacs-evil
-  :after treemacs)
-
-(add-hook 'treemacs-mode-hook (lambda () (treemacs-load-theme "all-the-icons")))
+;; (use-package lsp-treemacs
+;;   :after lsp)
+;; (use-package treemacs-projectile
+;;   :after (treemacs projectile)
+;;   :ensure t)
+;; (use-package treemacs-all-the-icons
+;;   :after treemacs)
+;; (use-package treemacs-evil
+;;   :after treemacs)
+;; 
+;; (add-hook 'treemacs-mode-hook (lambda () (treemacs-load-theme "all-the-icons")))
