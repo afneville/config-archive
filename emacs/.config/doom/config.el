68f4de923ce2b0325f3a8e9da816679bbceb5356
Alexander Neville <git@afneville.com>
2021-12-13 20:20:28 +0000

    minor changes to emacs config

--- emacs/.config/doom/config.el
+++ emacs/.config/doom/config.el
@@ -20,12 +20,15 @@
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
 (setq doom-font (font-spec :family "Iosevka Nerd Font" :size 14 :weight 'regular)
-      doom-variable-pitch-font (font-spec :family "Roboto" :size 16))
+      doom-variable-pitch-font (font-spec :family "CMU Typewriter Text" :size 15))
+      ;; doom-variable-pitch-font (font-spec :family "CMU Typewriter Text Variable Width" :size 15))
+      ;; doom-variable-pitch-font (font-spec :family "CMU Serif" :size 15))
+      ;; doom-variable-pitch-font (font-spec :family "CMU Bright" :size 15))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-gruvbox)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -68,6 +71,13 @@
 
 
 ;; ORG
+
+
+(setq
+ ispell-program-name "aspell"
+ ispell-local-dictionary "british-ise"
+ ispell-personal-dictionary "~/british-ise.pws"
+)
 
 (defun alex/org-mode-setup ()
   (font-lock-add-keywords 'org-mode
